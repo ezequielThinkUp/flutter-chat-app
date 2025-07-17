27fef72c034d58b2559f187930b671becd678f3b
ezequielThinkUp <tu-email-de-github@ejemplo.com>
2025-07-17 12:42:30 -0300

    Initial commit: Flutter chat app with null safety fixes

---
+++ android/app/src/main/kotlin/com/example/flutter_chat_final/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_chat_final
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/widgets/boton_azul.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class BotonAzul extends StatelessWidget {
+  final String text;
+  final Function onPressed;
+
+  const BotonAzul({Key? key, required this.text, required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        elevation: 2,
+        backgroundColor: Colors.blue,
+        shape: StadiumBorder(),
+      ),
+      onPressed: () => onPressed(),
+      child: Container(
+        width: double.infinity,
+        height: 55,
+        child: Center(
+          child: Text(this.text,
+              style: TextStyle(color: Colors.white, fontSize: 17)),
+        ),
+      ),
+    );
+  }
+}
